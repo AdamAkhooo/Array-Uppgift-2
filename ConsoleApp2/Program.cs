cefbad57eb876ab650df0f140159a7bf072e86c2
leonkarlsson1 <leka05001@utb.vaxjo.se>
2023-01-20 10:58:24 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hur många antal heltal vill du skriva in?");
+        int antal = int.Parse(Console.ReadLine());
+        string[] heltal = new string[antal];
+
+        for (int i = 0; i < antal; i++)
+        {
+            Console.WriteLine("skriv in valfri/valfria heltal");
+            heltal[i] = Console.ReadLine();
+
+        }
+
+        Console.WriteLine("Här kommer talen du valt visas");
+        for (int i = 0; i < antal; i++)
+        {
+            Console.Write($"tal{i + 1}): {heltal[i]} \n");
+        }
+    }
+}
